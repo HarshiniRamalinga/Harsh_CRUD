c6c4cb6c803edc7972cd8bdae2f172da80063de9
HarshiniRamalinga <91422976+HarshiniRamalinga@users.noreply.github.com>
2021-09-27 22:14:44 +0530

    Add files via upload

---
+++ CRUD/script.js
@@ -0,0 +1,66 @@
+var selectedRow = null
+
+function onFormSubmit() {
+   
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["age"] = document.getElementById("age").value;
+    formData["gender"] = document.getElementById("gender").value;
+    formData["hobby"] = document.getElementById("hobby").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.age;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.gender;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.hobby;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = ` <button id="btn1" onClick="onEdit(this)">Edit</button>
+                        <button id="btn2" onClick="onDelete(this)">Delete</button>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("gender").value = "";
+    document.getElementById("hobby").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("age").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("gender").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("hobby").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.age;
+    selectedRow.cells[2].innerHTML = formData.gender;
+    selectedRow.cells[3].innerHTML = formData.hobby;
+}
+
+function onDelete(td) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    
+}
